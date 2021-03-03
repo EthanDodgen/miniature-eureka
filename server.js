35289e9fef803e13b2012e9ab639d062242e7237
Fival <thefivalco@gmail.com>
2021-03-03 12:07:02 -0600

    update

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const path = require('path')
-const { db } = require("./Develop/db/db")
+const db = require("./Develop/db/db")
 
 const app = express()
 app.use(express.urlencoded({
